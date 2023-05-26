f5611e3a58321fc9e4049d283282b103abfac7e4
Brian Watkins <brian.watkins@gmail.com>
2023-05-26 17:48:55 -0400

    Move to esmodules

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true
+//   }
+// }
+
+// module.exports = nextConfig
+
+export default {
+  output: "standalone",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
+}
